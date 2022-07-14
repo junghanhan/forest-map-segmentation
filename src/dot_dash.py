50675b4f28d4e036c134c4e40dcfbacb84b53128
Jung-Han Han <hjhcw@naver.com>
2022-07-14 12:35:25 -0700

    Made some minor modification on Line Extraction code.

--- src/dot_dash.py
+++ src/dot_dash.py
@@ -1,8 +1,11 @@
 from shapely import affinity
-from shapely.geometry import Point, box, Polygon
+from shapely.geometry import Point, box, Polygon, LineString, MultiLineString
 from settings import DASH_SEARCH_BOX_W, DASH_SEARCH_BOX_H, MAX_DOT_LENGTH
-from line_proc import is_endpoint_inside, create_centerline
-
+from line_proc import is_endpoint_inside, get_line_endpoints, create_centerline, \
+    get_extrapolated_point, get_path_line, find_nearest_geom
+from shapely.ops import unary_union, linemerge
+from itertools import combinations
+from shapely.strtree import STRtree
 
 class Dash:
     all_dashes = {}  # key: id(Polygon), value: Dash instance
@@ -184,3 +187,115 @@ def create_dash_pairs(dot, dash_poly_pairs):
         dash_pairs.append((dash1, dash2))
 
     return dash_pairs
+
+
+def draw_dot_dashed_lines(dots, polygons, max_dot_length=0.0005):
+    """
+    Draws a solid vector line on dot-dash lines on the map.
+    While searching dots and dashes on the map, it also creates
+    instances of Dot and Dash classes.
+    TODO: need refactoring and performance optimization
+
+    :param dots: dots as shapely Points
+    :param polygons: all the polygons on the map as shapely Polygons
+    :param max_dot_length: maximum length of dot Shapely Polygon; used to ignore dot polygons
+        when dash polygons are searched
+    :return: connected dot-dashed lines as a Shapely MultiString
+    """
+
+    all_drawn_lines = []
+    poly_line_dict = {}  # id(polygon):centerline
+    dots_tree = STRtree(dots)
+    polygons_wo_dots = [poly for poly in polygons if len([dot for dot in dots_tree.query(poly) if
+                                                          poly.intersects(dot) and poly.length <= max_dot_length]) == 0]
+
+    # plotting non dot polygons
+    # for geom in polygons_wo_dots:
+    #   x,y = geom.exterior.xy # x,y are arrays
+    #   plt.plot(x,y)
+
+    polygons_wo_dots_tree = STRtree(polygons_wo_dots)
+
+    for p in dots:
+        dot = Dot(p)
+        dash_poly_pairs = dot.search_dash_polygons(polygons_wo_dots_tree, poly_line_dict)
+        if len(dash_poly_pairs) == 0:
+            del Dot.all_dots[id(p)]
+        else:
+            dash_pairs = create_dash_pairs(dot, dash_poly_pairs)  # Dash object is created
+            dot.save_dash_pairs(dash_pairs)
+
+    # make virtual dots (dots not detected) for dashes having dots less than two
+    dashes_copy = list(Dash.all_dashes.values())
+    for dash in dashes_copy:
+        if len(dash.conn_dots) < 2:
+            if id(dash.poly) in poly_line_dict:
+                dash_body_line = poly_line_dict[id(dash.poly)]
+            else:
+                dash_body_line = create_centerline(dash.poly)
+                poly_line_dict[id(dash.poly)] = dash_body_line
+
+            if dash_body_line is not None:
+                endpoints = get_line_endpoints(dash_body_line)
+
+                # filter out the endpoints close to valid dots
+                # because virtual dot is no need at that end
+                endpoints_wo_dots = endpoints.copy()
+                for dot in dash.conn_dots:
+                    min_dist_ep = find_nearest_geom(dot.point, endpoints)
+                    endpoints_wo_dots.remove(min_dist_ep)
+
+                # make virtual dots at the extrapolated location
+                # find dashes around the virtual dots
+                for ep in endpoints_wo_dots:
+                    target_p = get_extrapolated_point(dash_body_line, ep)
+                    dot = Dot(target_p)
+                    dash_poly_pairs = dot.search_dash_polygons(polygons_wo_dots_tree, poly_line_dict)
+                    if len(dash_poly_pairs) == 0:
+                        del Dot.all_dots[id(target_p)]
+                    else:
+                        dash_pairs = create_dash_pairs(dot, dash_poly_pairs)  # Dash object is created
+                        dot.save_dash_pairs(dash_pairs)
+
+    # obtain dash lines
+    for dash in Dash.all_dashes.values():
+        if id(dash.poly) in poly_line_dict:
+            dash_body_line = poly_line_dict[id(dash.poly)]
+        else:
+            dash_body_line = create_centerline(dash.poly)
+            poly_line_dict[id(dash.poly)] = dash_body_line
+
+        if dash_body_line is not None:
+            endpoints = get_line_endpoints(dash_body_line)
+
+            # prepare dash body line to be merged
+            if isinstance(dash_body_line, MultiLineString):
+                lines = list(dash_body_line.geoms)
+            else:
+                lines = [dash_body_line]
+
+            # make the connecting line from dash's endpoint to dot
+            for dot in dash.conn_dots:
+                min_dist_ep = find_nearest_geom(dot.point, endpoints)
+                lines.append(LineString([min_dist_ep, dot.point]))
+
+            # merged line with the dash body line and connecting line to dots
+            mline = linemerge(lines)
+
+            # find shortest paths between dots
+            path_lines = [get_path_line(mline, dot1.point, dot2.point) for dot1, dot2 in
+                          combinations(dash.conn_dots, 2)]
+
+            if len(path_lines) > 0:
+                dash_line = unary_union(path_lines)
+                dash.dash_line = dash_line
+
+                # plot the final line for dash
+                #plot_line(dash_line)
+
+                if isinstance(dash_line, MultiLineString):
+                    all_drawn_lines.extend(list(dash_line.geoms))
+                else:  # LineString
+                    all_drawn_lines.append(dash_line)
+
+    return linemerge(all_drawn_lines)
